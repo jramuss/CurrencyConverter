86091d8bb1d7e1293736baeabecb3f19c798831c
jramuss <josuedasilvaramus@hotmail.com>
2024-07-08 10:45:58 -0400

    CurrencyConverter

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,11 @@ import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: const Home(),
     theme: ThemeData(
         hintColor: Colors.amber,
         primaryColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
+        inputDecorationTheme: const InputDecorationTheme(
           enabledBorder:
               OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
           focusedBorder:
@@ -19,7 +19,7 @@ void main() {
   ));
 }
 
-final url = 'http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL';
+const url = 'http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL';
 
 Future <Map<String,dynamic>>getData() async {
   final response = await http.get(Uri.parse(url));
@@ -85,7 +85,7 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.black87,
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
-          title: Text(
+          title: const Text(
             ('\$Conversor\$'),
             style: TextStyle(color: Colors.amber),
           ),
@@ -95,33 +95,33 @@ class _HomeState extends State<Home> {
             future: getData(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return (Center(
+                return (const Center(
                   child: CircularProgressIndicator(
                     backgroundColor: Colors.blueGrey,
                   ),
                 ));
               } else if (snapshot.hasError) {
                 return Text('Erro: ${snapshot.error}',
-                    style: TextStyle(color: Colors.white));
+                    style: const TextStyle(color: Colors.white));
               } else {
                 dolar = double.parse(snapshot.data!['USDBRL'] ['high'].toString());
                 euro = double.parse(snapshot.data!['EURBRL'] ['high'].toString());
 
 
                 return SingleChildScrollView(
-                  padding: EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Icon(
+                      const Icon(
                         Icons.monetization_on,
                         size: 150.0,
                         color: Colors.amber,
                       ),
                         buildTextField('Reais', 'R\$', realController, _realChanged),
-                      Divider(),
+                      const Divider(),
                         buildTextField('Dólares', 'US\$', dolarController,_dolarChanged),
-                      Divider(),
+                      const Divider(),
                       buildTextField('Euros', '€', euroController, _euroChanged),
                     ],
                   ),
@@ -135,11 +135,11 @@ class _HomeState extends State<Home> {
     controller: c,
     decoration: InputDecoration(
       labelText: label,
-      labelStyle: TextStyle(color: Colors.amber),
-      border: OutlineInputBorder(),
+      labelStyle: const TextStyle(color: Colors.amber),
+      border: const OutlineInputBorder(),
       prefixText: prefix,
     ),
-    style: TextStyle(
+    style: const TextStyle(
         color: Colors.amber, fontSize: 25
     ),
     onChanged: (texto){
